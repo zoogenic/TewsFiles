f6b8355ca9287695fde4396a204486d89226a07a
zoogenic <zoogenic@gmail.com>
2015-03-29 02:07:47 +0300

    program options parsing; output directory processing

--- src/tewsfiles.cpp
+++ src/tewsfiles.cpp
@@ -1,11 +1,16 @@
 #define CURL_STATICLIB
+
+// STD
 #include <iostream>
-#include <curl/curl.h>
 
 // BOOST
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/range/adaptors.hpp>
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+
+#include <curl/curl.h>
 
 namespace bpt = boost::property_tree;
 
@@ -20,7 +25,7 @@ namespace Private
         return written;
     }
 
-    CURLcode downloadUrl(const std::string &url)
+    CURLcode downloadUrl(const std::string &url, const char *out)
     {
         CURL *curl;
         FILE *fp;
@@ -28,7 +33,7 @@ namespace Private
         curl = curl_easy_init();
         if (curl)
         {
-            fp = fopen(outfilename,"wb");
+            fp = fopen(out,"wb");
             curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
             curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
             curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
@@ -58,22 +63,57 @@ namespace Private
     } // namespace Predicate
 } //namespace Private
 
-int main()
+int main(int argc, char *argv[])
 {
+    namespace bpo = boost::program_options;
     namespace bpxml = boost::property_tree::xml_parser;
     namespace ba = boost::adaptors;
+    namespace bfs = boost::filesystem;
+
+    // processing command line arguments -- begin
+    bpo::options_description desc("Program options:");
+    std::string output_dir_str;
+    desc.add_options()
+        ("help,h", "Show help")
+        ("output,o", bpo::value<std::string>(&output_dir_str), "Output folder")
+        ;
+    bpo::variables_map vm;
+
+    try
+    {
+        const bpo::parsed_options parsed = bpo::command_line_parser(argc, argv).options(desc).run();
+        bpo::store(parsed, vm);
+    }
+    catch(const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+
+    bpo::notify(vm);
+    if (vm.count("help"))
+    {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+    // processing command line arguments -- end
+
+    bfs::path output_dir(output_dir_str); // TODO: is ok path?
 
     static const std::string rss_url = "http://downloads.bbc.co.uk/podcasts/worldservice/tae/rss.xml";
-    Private::downloadUrl(rss_url);
+    Private::downloadUrl(rss_url, Private::outfilename);
 
     bpt::ptree pt;
     bpt::read_xml(Private::outfilename, pt, bpxml::trim_whitespace | bpxml::no_comments);
 
+    static const std::string podcast_head = "http://www.bbc.co.uk/learningenglish/english/features/the-english-we-speak/ep-";
     for (auto item: pt.get_child("rss.channel") | ba::filtered(Private::Predicate::CheckTag("item")))
     {
         for (auto prop: item.second | ba::filtered(Private::Predicate::CheckTag("link")))
         {
-            std::cout << prop.second.get_value<std::string>() << std::endl;
+            const std::string suburl = prop.second.get_value<std::string>().substr(59, 6);
+            Private::downloadUrl(podcast_head + suburl
+                    , (output_dir / (suburl + ".html")).string().c_str());
         }
     }
 }
